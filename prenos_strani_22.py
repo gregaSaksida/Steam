6ac86dc8e71f6cb8dcc3271f3c4d96f9570aeb49
gregaSaksida <grega.saksida@student.fmf.uni-lj.si>
2018-08-16 21:41:07 +0200

    Povsem dokončano. Odpravljene zadnje napake v regularnem izrazu.

--- prenos_strani_22.py
+++ prenos_strani_22.py
@@ -8,24 +8,29 @@ pot = 'spletne_strani'  #ime mape s spletnimi stranmi
 
 def prenesi_stran(url, ime):
     """Funkcija prenese spletno stran in jo shrani pod danim imenom."""
-    os.makedirs(pot, exist_ok=True)
-    stran = requests.get(url)
+    os.makedirs('igre', exist_ok=True)
+    stran = requests.get(url, auth=('user', 'pass'))
     ime = ime + '.html'
-    with open(os.path.join(pot, ime),
+    #print(os.path.join(pot, ime))
+    with open(os.path.join('igre', ime),
               'w', encoding='utf-8') as datoteka:
         datoteka.write(stran.text)
 			
 
 iskani_niz = re.compile(
-	r'(<div style="clear: left;"></div>|<!-- End Extra empty div -->).*?<a href="(?P<url>http.*?://store.steampowered.com/(app|sub)/.*?)"  data-ds.*?'  #http.*? zaobjame http in https
-	r'<span class="title">(?P<ime>.+?)</span>'
-	r'.*?'
-	r'row">(?P<datum>\d{1,2} [A-Z][a-z]{2}, (?P<leto>\d{4}))</div>'
-	r'.*?'
-	r'br&gt;(?P<odstotek>\d{1,2})% of the (?P<stevilo_ocen>(\d|,)*) user'  #delez dobrih ocen in stevilo vseh ocen
-	r'.*?</span>',
+        r'(<div style="clear: left;"></div>|<!-- End Extra empty div -->).{0,50}?'
+        r'<a href="(?P<url>http.{0,1}?://store.steampowered.com/'  #http.{0,1} zaobjame http in https
+        r'(app|sub)/.{0,400}?)"data-ds.{0,800}?'
+        r'<span class="title">(?P<ime>.{1,100}?)</span>'
+        r'.{0,700}?'
+        r'row">(?P<datum>\d{1,2} [A-Z][a-z]{2}, (?P<leto>\d{4}))</div>'  #datum je oblike 12 Jan, 2016
+        r'.{100,300}?'
+        r'br&gt;(?P<odstotek>\d{1,2})% of the (?P<stevilo_ocen>(\d|,){0,20}?) user reviews for this game',  #delez dobrih ocen in stevilo vseh ocen
 	flags=re.DOTALL
 )
+"""Na vsaki strani pricakujem 15-20 zadetkov. Če uporabim zgolj .*?, namesto da
+navedem tocno stevilo znakov, lahko vec iger prepozna kot en zadetek
+(nekatere igre recimo nimajo zapisanega stevila ocen)."""
 
 
 slovar_iger = {}
@@ -34,21 +39,22 @@ slovar_iger = {}
 for index in range(1, 335):
     datoteka = open(os.path.join(pot, 'stran_{}.html'.format(index)), 'r', 
                     encoding='utf-8')
-    print(os.path.join(pot, 'stran_{}.html'.format(index)))  #Izpise, katero stran trenutno obdeluje.
+    #print(os.path.join(pot, 'stran_{}.html'.format(index)))  #Izpise, katero stran trenutno obdeluje.
     vsebina = datoteka.read()
-    stevec=1
     for ujemanje in iskani_niz.finditer(vsebina):
-        stevec +=1
-        if (stevec == 23) and index == 16:
-            print(ujemanje.group('ime'))
-            print(ujemanje.group('url'))
         stevilo_ocen = int(ujemanje.group('stevilo_ocen').replace(',', ''))
         """ce je stevilo ocen veliko, je podano z vejico, npr. 10,314."""
         if (int(ujemanje.group('leto')) <= 2017 and 
             stevilo_ocen >= 1000):
             """Zavrze najnovejse igre in igre s premalo ocenami."""
-            slovar_iger[ujemanje.group('url')] = ujemanje.group('ime')
+            ime = ujemanje.group('ime').replace('/',',').replace(':',',').replace('*',',').replace('?',',').replace('<',',').replace('>',',').replace('"',',').replace('\\',',')
+            slovar_iger[ime] = ujemanje.group('url')
     datoteka.close()
 
-					
-					
+stevec = 1
+print(len(slovar_iger))
+
+for ime in slovar_iger:
+    prenesi_stran(slovar_iger[ime], ime)
+    print(stevec)
+    stevec += 1
