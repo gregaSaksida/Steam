d44094df4aa7ed7339f87692453ac61f8a0ce278
gregaSaksida <grega.saksida@student.fmf.uni-lj.si>
2018-08-18 11:50:54 +0200

    Posodobljena .json datoteka. Nove .csv datoteke. "podatki_strani" sedaj izvozi tudi csv datoteko.

--- podatki_strani.py
+++ podatki_strani.py
@@ -7,6 +7,8 @@ Created on Sun Aug 12 12:44:59 2018
 
 import os
 import re
+import json
+import csv
 
 pot = 'igre'
 
@@ -30,7 +32,7 @@ iskani_niz_1 = re.compile(
         r'.*?<div class="game_purchase_price price">.{1,20}?'
         r'(?P<cena>Free to Play|\d{1,3},\d{1,2})'
         r'.*?System Requirements.*?Minimum:.*?Memory:</strong> (?P<RAM>\d{1,4} (K|M|G)B) RAM'
-        r'.*?Storage:</strong> (?P<prostor>\d{1,4} (K|M|G)B) available space',
+        r'.*?Storage:</strong> (?P<prostor>\d{1,4} (K|M|G)B) ',
         flags=re.DOTALL
 )
 
@@ -53,11 +55,13 @@ iskani_niz_2 = re.compile(
         r'(?P<kategorije>\w.*?)\t{12}</a><div class="app_tag add_button"'
         r'.*?<b>Genre:</b>.*?">(?P<zanri>.*?)</a><br>'
         r'.*?<div class="game_purchase_price price">.{1,20}?'
-        r'(?P<cena>Free to Play|\d{1,3},\d{1,2})',
+        r'(?P<cena>Free to Play|Free|\d{1,3},\d{1,2})',
         flags=re.DOTALL
 )
 
 seznam_iger = []
+seznam_kategorij = []
+seznam_zanrov = []
 stevec = 1
 
 """Odpre stran z opisom igre"""
@@ -67,8 +71,10 @@ for igra in os.listdir(pot):
     vsebina = datoteka.read()
     
     """Poišče ujemanje, najprej poskusi strozjega."""
+    niz_1 = False  # S to spremenljivko belezim, kateri od obeh nizov je bil najden.
     if iskani_niz_1.search(vsebina):
         ujemanje = iskani_niz_1.search(vsebina).groupdict()
+        niz_1 = True
     elif iskani_niz_2.search(vsebina):
         ujemanje = iskani_niz_2.search(vsebina).groupdict()
     else:
@@ -90,6 +96,8 @@ for igra in os.listdir(pot):
         while posebni_niz_1.search(kategorije):
             kategorije = re.sub(posebni_niz_1, ',', kategorije)
         ujemanje['kategorije'] = kategorije.split(',')
+        for kategorija in ujemanje['kategorije']:
+            seznam_kategorij.append({'ime': igra, 'kategorija': kategorija})
         
         posebni_niz_2 = re.compile(
                 r'</a>, <a href=".*?">',
@@ -98,15 +106,62 @@ for igra in os.listdir(pot):
         while posebni_niz_2.search(zanri):
             zanri = re.sub(posebni_niz_2, ',', zanri)
         ujemanje['zanri'] = zanri.split(',')
+        for zanr in ujemanje['zanri']:
+            seznam_zanrov.append({'ime': igra, 'zanr': zanr})
         
         ujemanje['ime'] = igra
+        
+        if ujemanje['cena'] == 'Free to Play' or ujemanje['cena'] == 'Free':
+            ujemanje['cena'] = '0,0'
+        else:
+            cena = ujemanje['cena'].split(',')
+            ujemanje['cena'] = int(cena[0]) + int(cena[1]) / 100
+        
+        """Pretvori zahteve za RAM in trdi disk v MB."""
+        if niz_1:
+            ram = ujemanje['RAM'].split()
+            hd = ujemanje['prostor'].split()
+            if ram[1] == 'GB':
+                ujemanje['RAM'] = int(ram[0]) * 1024
+            elif ram[1] == 'MB':
+                ujemanje['RAM'] = int(ram[0])
+            elif ram[1] == 'KB':
+                ujemanje['RAM'] = int(ram[0]) / 1024
+            if hd[1] == 'GB':
+                ujemanje['prostor'] = int(hd[0]) * 1024
+            elif hd[1] == 'MB':
+                ujemanje['prostor'] = int(hd[0])
+            elif hd[1] == 'KB':
+                ujemanje['prostor'] = int(hd[0]) / 1024
+        
+        """Stevilo ocen pretvori iz zapisa z vejicami v navaden zapis."""
+        stevilo_ocen = ujemanje['stevilo_ocen'].split(',')
+        stevilo = 0
+        faktor = len(stevilo_ocen) - 1
+        for element in stevilo_ocen:
+            stevilo += int(element) * (1000**faktor)
+            faktor -= 1
+        ujemanje['stevilo_ocen'] = stevilo
+        
         seznam_iger.append(ujemanje)
     
     print(stevec)
     stevec += 1
 
-import json
-with open('igre.json', 'w') as datoteka:
+with open('igre.json', 'w', encoding='utf-8') as datoteka:
     json.dump(seznam_iger, datoteka, indent=2)
+
+def zapisi_csv(podatki, polja, ime_datoteke):
+    with open(ime_datoteke, 'w', encoding='utf-8') as datoteka:
+        pisalec = csv.DictWriter(datoteka, polja, extrasaction='ignore')
+        pisalec.writeheader()
+        for podatek in podatki:
+            pisalec.writerow(podatek)
+
+polja = ['ime', 'leto', 'odstotek', 'stevilo_ocen', 
+         'cena', 'razvijalec', 'izdajatelj', 'RAM', 'prostor']
+zapisi_csv(seznam_iger, polja, 'igre.csv')
+zapisi_csv(seznam_kategorij, ['ime', 'kategorija'], 'kategorije.csv')
+zapisi_csv(seznam_zanrov, ['ime', 'zanr'], 'zanri.csv')
         
         
